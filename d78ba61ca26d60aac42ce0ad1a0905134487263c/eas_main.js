6a5d938626bf75b0f0e824e79e5bbb820fb62369
TheEndBoss_101 <TheEndBoss101@gmail.com>
2022-04-25 22:50:38 -0500

    Update

--- d78ba61ca26d60aac42ce0ad1a0905134487263c/eas_main.js
+++ d78ba61ca26d60aac42ce0ad1a0905134487263c/eas_main.js
@@ -1,11 +1,30 @@
 alert("WARNING: HIGHLY EXPERMENTAL!")
 
 function sendMessage() {
+    const ORG = originator.value
+    console.log(ORG)
+    const EEE = events.value
+    console.log(EEE)
+    const P = subdiv.value
+    console.log(P)
+    const SS = state.value
+    console.log(SS)
+    const CCC = county.value
+    console.log(CCC)
+    const PSSCCC = P+SS+CCC
+    console.log(PSSCCC)
+    const HHMM = purgetime.value
+    console.log(HHMM)
+    const LLLLLLLL = senderCallsign.value
+    console.log(LLLLLLLL)
+    const MSG = message.value
+    console.log(MSG)
+
     const request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/965359101926314025/H6RpMxAvESc9GQ3ip-wPVCqxSgeBwknVHaWZSE3v6PiaRmsw7GGBlqkINK7ff12V9M3g");
     request.setRequestHeader('Content-type', 'application/json');
     const params = {
-        content: "EAS EVENT: `${#originator}.value-${#event}.value-${#subdiv}.value${#state}.value${#county}.value-${#purgetime}.value-${#senderCallsign}.value+${#message}.value` Transcript: `${#message}.value`",
+        content: `EAS EVENT: "${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}+${MSG}" Transcript: "${MSG}`,
         ttss: true
     }
 
