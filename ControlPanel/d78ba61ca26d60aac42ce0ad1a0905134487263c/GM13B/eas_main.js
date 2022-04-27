7097911a5c65206b13c3a70cac979756f4d750d8
TheEndBoss_101 <TheEndBoss101@gmail.com>
2022-04-26 21:15:17 -0500

    test

--- ControlPanel/d78ba61ca26d60aac42ce0ad1a0905134487263c/GM13B/eas_main.js
+++ ControlPanel/d78ba61ca26d60aac42ce0ad1a0905134487263c/GM13B/eas_main.js
@@ -117,5 +117,15 @@ function sendRMT() {
 }
 
 function generate() {
+    const ORG = originator.value
+    const EEE = events.value
+    const P = subdiv.value
+    const SS = state.value
+    const CCC = county.value
+    const PSSCCC = P+SS+CCC
+    const HHMM = purgetime.value
+    const LLLLLLLL = senderCallsign.value
+    const MSG = message.value
+    
 	$("#code").innerText = `${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}`;
 }
