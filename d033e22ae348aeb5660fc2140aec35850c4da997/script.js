ffec32c99f397a2feb2f655226489d4fe3795b29
TheEndBoss_101 <TheEndBoss101@gmail.com>
2022-06-21 16:19:36 -0500

    2.1.1

--- d033e22ae348aeb5660fc2140aec35850c4da997/script.js
+++ d033e22ae348aeb5660fc2140aec35850c4da997/script.js
@@ -1,4 +1,4 @@
-console.log("Emergency Alert System - V:2.1.1")
+console.log("Emergency Alert System - V:2.1.2")
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
@@ -71,7 +71,7 @@ function Send_EAS_Message_Discord() {
         params.tts = false
     }
     if (document.getElementById('AtRole').checked) {
-        params.content = `@EAS-OPT \n :rotating_light:** === *EAS MESSAGE* ===** :rotating_light:\nHeader: \`${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}\`\nMessage: \`${MSG}\``
+        params.content = `<@988911824839790643> \n :rotating_light:** === *EAS MESSAGE* ===** :rotating_light:\nHeader: \`${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}\`\nMessage: \`${MSG}\``
         params.allowed_mentions = { "roles": ["988911824839790643"] }
     }
     else{
@@ -162,7 +162,7 @@ function Send_RMT_Message_Discord() {
         params.tts = false
     }
     if (document.getElementById('AtRole').checked) {
-        params.content = `@EAS-OPT \n :rotating_light:** === *EAS MESSAGE* ===** :rotating_light:\nHeader: \`${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}\`\nMessage: \`${MSG}\``
+        params.content = `<@988911824839790643> \n :rotating_light:** === *EAS MESSAGE* ===** :rotating_light:\nHeader: \`${ORG}-${EEE}-${PSSCCC}-${HHMM}-${LLLLLLLL}\`\nMessage: \`${MSG}\``
         params.allowed_mentions = { "roles": ["988911824839790643"] }
     }
     else{
